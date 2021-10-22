77d88f6928b094e8435e4d854fa233c28c22deb3
Dana Sugu <dana.sugu@gmail.com>
2021-10-22 17:18:01 +0300

    Update index.php

--- application/views/calendar/index.php
+++ application/views/calendar/index.php
@@ -19,3 +19,12 @@ function getEvents(date){
     });
 }
 </script>
+
+<script>
+$(document).on("change", ".month-dropdown", function(){
+    getCalendar('calendar_div', $('.year-dropdown').val(), $('.month-dropdown').val());
+});
+$(document).on("change", ".year-dropdown", function(){
+    getCalendar('calendar_div', $('.year-dropdown').val(), $('.month-dropdown').val());
+});
+</script>
